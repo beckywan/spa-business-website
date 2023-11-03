5eb6d733a9b1d3db13550ba892bbdcd6e384b526
beckywan <beckywang04@gmail.com>
2023-11-03 19:00:26 -0400

    Fixed image issue and added images

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -20,14 +20,14 @@ export default function Contact() {
                     <h2>Directions</h2>
                     <p>New York, NY</p>
                 </div>
-                <div class="google-map">
+                <div className="google-map">
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387194.0622236789!2d-74.30932477148002!3d40.69701929469058!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1698952436674!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
                 <br></br>
-                <div>
+                <div className="instagram">
                     <h2>Find more on Instagram</h2>
                     <a href="https://www.google.com/">
-                        <img src="images/instagram-icon.png" alt="@nailSalon" />
+                        <img src="/images/instagram-icon.png" alt="@nailSalon"/>
                     </a>
                 </div>
             </section> 
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -2,8 +2,8 @@ export default function Home() {
     return <body>
     <section className="home"> 
         <h1>Nail Care, Spa, Beauty</h1>
-        {/* <img>src="" alt="Beautiful nails"</img> */}
-        <div>
+        <div className="first-nails">
+            <img src="/images/fall-nails.jpg" alt="Image of beautiful nails by us"/>
             <p>Taking care of your nails is a part of taking care of yourself. Do something for you by checking out Nail Spa. </p>
         </div>
     </section> 
